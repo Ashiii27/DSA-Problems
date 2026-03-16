544b56d319b0d72c8658f5414b25c38fc3707f30
Ashiii27 <ashiii.2790@gmail.com>
2026-03-16 20:52:47 +0530

    some more conditional statements

--- MERN/JS_FOUNDATION/condition-challenges.js
+++ MERN/JS_FOUNDATION/condition-challenges.js
@@ -2,7 +2,6 @@
  
 // let num1 = 10;
 // let num2 = 12;
-
 // if (num1 > num2) {
 //     console.log("num1 is greater");
 // } else {
@@ -13,17 +12,44 @@
 
 // let username1 = "ashi";
 // let username2 = "ashi";
-
 // if (username1 == username2) {
 //     console.log("Pick another username");
 // } else {
 //     console.log("Username approved");
 // }
 
+
+
 //difference between double and triple equals too
+
 // let x = 10;
 // let y = "10";
-
 // console.log(x == y);  // true  it converts the data types and then gives the result
 // console.log(x === y); // false  it gives the result strictly there is no conversion of data types
 
+//check the data type
+
+// let a = "ashi"; //string
+// if(typeof a === "string") {
+//     console.log("a is a string");
+// } else {
+//     console.log("a is not a string");
+// }
+
+//checking if boolean value is true or false
+
+// let isTeaReady = false;
+// if (isTeaReady) {
+//     console.log("tea is ready");
+// } else {
+//     console.log("tea is not ready");
+// }
+
+//check is array is empty or not
+
+let items = [];
+if (items.length === 0) {
+    console.log("empty");
+} else {
+    console.log("yes it is not empty");
+}
