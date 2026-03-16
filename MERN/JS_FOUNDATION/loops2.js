ca3892de4d4fa871e40c6d05de54117ecc0b2a97
Ashiii27 <ashiii.2790@gmail.com>
2026-03-16 23:49:52 +0530

    loops part 2

---
+++ MERN/JS_FOUNDATION/loops2.js
@@ -0,0 +1,27 @@
+/*
+1.Write a for loop that loops through the array [1, 2, 3, 4, 5] and stops when it reaches the number 3.Stores all the numbers before 3 in a new array named 'numbersBeforeThree'.
+*/
+
+// let numbersBeforeThree = [];
+// for (let i = 0; i < 5; i++) {
+//     if (i === 2) {
+//         break;
+//     }
+//     numbersBeforeThree.push(i + 1);
+// }
+// console.log(numbersBeforeThree);
+
+/*
+2.Write a for loop that loops through the array ["london", "tokyo", "paris", "berlin"] and skips "paris"
+*/
+
+// let cities = ["london", "tokyo", "paris", "berlin"];
+// let cityList = [];
+// for (let i = 0; i < cities.length; i++) {
+//     if (cities[i] === "paris") {
+//         continue;
+//     }
+//     cityList.push(cities[i]);
+// }
+// console.log(cityList);
+
