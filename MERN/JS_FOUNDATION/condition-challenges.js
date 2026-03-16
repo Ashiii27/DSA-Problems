d887968f2ad8483fbc535e89035067cc8cfc58d0
Ashiii27 <ashiii.2790@gmail.com>
2026-03-16 20:43:25 +0530

    if else in strings

--- MERN/JS_FOUNDATION/condition-challenges.js
+++ MERN/JS_FOUNDATION/condition-challenges.js
@@ -1,11 +1,29 @@
 //check if a number is greater
  
-let num1 = 10;
-let num2 = 12;
+// let num1 = 10;
+// let num2 = 12;
 
-if (num1 > num2) {
-    console.log("num1 is greater");
-} else {
-    console.log("num2 is greater");
-    
-}
+// if (num1 > num2) {
+//     console.log("num1 is greater");
+// } else {
+//     console.log("num2 is greater");
+// }
+
+//check if strings are equal or not
+
+// let username1 = "ashi";
+// let username2 = "ashi";
+
+// if (username1 == username2) {
+//     console.log("Pick another username");
+// } else {
+//     console.log("Username approved");
+// }
+
+//difference between double and triple equals too
+// let x = 10;
+// let y = "10";
+
+// console.log(x == y);  // true  it converts the data types and then gives the result
+// console.log(x === y); // false  it gives the result strictly there is no conversion of data types
+
