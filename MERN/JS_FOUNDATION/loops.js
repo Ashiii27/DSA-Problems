f068586aa5719989136e110fc590ac970afc16d4
Ashiii27 <ashiii.2790@gmail.com>
2026-03-16 23:32:45 +0530

    fun with loops in javascript

---
+++ MERN/JS_FOUNDATION/loops.js
@@ -0,0 +1,61 @@
+//Write a 'while' loop that calculates the sum of all numbers from 1 to 5 and stores the result in a variable named 'sum'.
+
+// let sum = 0;
+// let i = 1;
+// while (i <= 5) {
+//     sum = sum + i;
+//     i++;
+// }
+// console.log(sum);
+
+//write a 'while' loop that counts from 5 to 1 and stores the numbers in an array named 'countdown'
+
+// let countdown = [];
+// let i = 5;
+// while (i >= 1) {
+//     countdown.push(i);
+//     i--;
+// }
+// console.log(countdown);
+
+//write a 'do while' loop that prompts a user to enter their favourite tea type in array named 'teaCollection'.
+
+// let teaCollection = [];
+// let userInput;
+// do {
+//     userInput = prompt("Enter your favourite tea type (or 'quit' to stop):");
+//     if (userInput !== "quit") {
+//         teaCollection.push(userInput);
+//     }
+// } while (userInput !== "quit");
+// console.log(teaCollection);
+
+//Write a 'do while' loop that adds numbers from 1 to n to a variable named 'total'.
+
+// let total = 0;
+// let n;
+// n = prompt("Enter a number:");
+// let i=1;
+// do {
+//     total = total + i;
+//     i++;
+// } while (i <= n);
+// console.log(total);
+
+//write a for loop that multiplies each element in the array `[2,4,6]` by 2 and stores the results in a new array named 'multipliedArray'.
+
+// let arr = [2, 4, 6];
+// let multipliedArray = [];
+// for (let i = 0; i < arr.length; i++) {
+//     multipliedArray.push(arr[i] * 2);
+// }
+// console.log(multipliedArray);
+
+//write a for loop that lists all the cities in array["New York", "Los Angeles", "Chicago"] and stores each city in a new array named `cityList`.
+
+// let cities = ["New York", "Los Angeles", "Chicago"];
+// let cityList = [];
+// for (let i = 0; i < cities.length; i++) {
+//     cityList.push(cities[i]);
+// }
+// console.log(cityList);
