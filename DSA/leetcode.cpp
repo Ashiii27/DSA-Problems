b2039dc502c1e0428e135988f4cd5fbad8755c45
Ashiii27 <ashiii.2790@gmail.com>
2026-03-20 16:36:26 +0530

    leetcode problems date - 20th march 2026

---
+++ DSA/leetcode.cpp
@@ -0,0 +1,8 @@
+//Sort arrays of 0s 1s and 2s
+    void sortColors(vector<int>& nums) {
+    int low{}, mid{}, high{nums.size() - 1};
+    while (mid <= high)
+        if (nums[mid] == 0) swap(nums[mid++], nums[low++]);
+        else if (nums[mid] == 1) mid++;
+        else swap(nums[mid], nums[high--]);
+}
