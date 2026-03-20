73e467a6410ae55f7a8d4fdd1f6a744cbbe4e225
Ashiii27 <ashiii.2790@gmail.com>
2026-03-20 23:30:02 +0530

    oops concepts

--- MERN/JS_FOUNDATION/oop.js
+++ MERN/JS_FOUNDATION/oop.js
@@ -8,7 +8,7 @@ let car = {
     }
 }
 
-console.log(car.start());
+//console.log(car.start());
 
 //constructor function
 
@@ -29,5 +29,31 @@ Person.prototype.greet = function() {
     return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
 };
 
-console.log(person1.greet()); // Output: Hello, my name is Alice and I am 30 years old.
-console.log(person2.greet()); // Output: Hello, my name is Bob and I am 25 years old.
+//console.log(person1.greet()); // Output: Hello, my name is Alice and I am 30 years old.
+//console.log(person2.greet()); // Output: Hello, my name is Bob and I am 25 years old.
+
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    start() {
+        return `The ${this.year} ${this.make} ${this.model} is starting.`;
+    }
+}
+
+class Car extends Vehicle {
+    drive() {
+        return `The ${this.year} ${this.make} ${this.model} is driving.`; //inheritence example
+    }
+}
+
+let myCar = new Car("toyota", "camry", 2020);
+//console.log(myCar.start());
+//console.log(myCar.drive());
+
+let vehOne= new Vehicle("Honda", "Civic", 2020);
+//console.log(vehOne.start());
+
