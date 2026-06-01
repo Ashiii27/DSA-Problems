223bfd9dda00c4f744c31b54e6aeb5cc98f8d354
Ashish Kumar <ashiii.2790@gmail.com>
2026-06-01 21:24:48 +0530

    Time: 220 ms (99.71%), Space: 20 MB (71.64%) - LeetHub

--- 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.py
+++ 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.py
@@ -1,28 +1,101 @@
 class Solution:
     def getBiggestThree(self, grid: List[List[int]]) -> List[int]:
-        m, n = len(grid), len(grid[0])
-        max_length = min((m - 1) // 2, (n - 1) // 2)
-        best = []
-        heapify(best)
-        def rhombusSum(r, c, k):
-            result = grid[r][c]
-            for s in range(k):
-                r += 1
-                result += grid[r][c + s + 1]
-                result += grid[r][c - s - 1]
-            for s in range(k - 1, -1, -1):
-                r += 1
-                result += grid[r][c + s]
-                if s > 0:
-                    result += grid[r][c - s]
-            return result
-        for r in range(m):
-            for c in range(n):
-                for k in range(max_length + 1):
-                    if c >= k and c + k < n and r + 2 * k < m:
-                        candidate = rhombusSum(r, c, k)
-                        if candidate not in set(best):
-                            heappush(best, candidate)
-                        if len(best) > 3:
-                            heappop(best)
-        return sorted(best, reverse = True)
+        for row in grid: print(row)
+        width = len(grid[0])
+        height = len(grid)
+
+        upRight = [[0]*(width + 1) for _ in range(height + 1)]
+        downRight = [[0]*(width + 1) for _ in range(height + 1)]
+        
+        # fill upRight
+        for diag in range(width + height - 1):
+            y = min(diag, height - 1)
+            x = diag - y
+            curDiag = 0
+
+            while y >= 0 and x < width:
+                curDiag += grid[y][x]
+                upRight[y][x + 1] = curDiag
+                x += 1
+                y -= 1
+
+  
+        # fill downRight
+        for diag in range(-height + 1, width):
+            # diag = x - y
+            x = max(diag, 0)
+            y = x - diag
+            curDiag = 0
+
+            while y < height and x < width:
+                curDiag += grid[y][x]
+                downRight[y + 1][x + 1] = curDiag
+                x += 1
+                y += 1
+
+
+
+
+        def getUpRightSum(startX,startY, sideLen):
+            return upRight[startY - sideLen + 1][startX + sideLen] - upRight[startY + 1][startX] 
+        
+        def getDownRightSum(startX,startY, sideLen):
+            return downRight[startY + sideLen][startX + sideLen] - downRight[startY][startX]
+
+
+        def getRhombusSum(startX, startY, sideLen):
+
+            sw = downRight[startY + sideLen][startX + sideLen] - downRight[startY][startX]
+            # sw = getDownRightSum(startX, startY, sideLen)
+            nw = upRight[startY - sideLen][startX + 1 + sideLen] - upRight[startY][startX + 1] 
+            # nw = getUpRightSum(startX + 1, startY - 1, sideLen)
+            se = downRight[startY + 1][startX + sideLen + 1 + sideLen] - downRight[startY - sideLen + 1][startX + sideLen + 1]
+            # se = getDownRightSum(startX + sideLen + 1, startY - sideLen + 1, sideLen)
+            ne = upRight[startY + 1][startX + sideLen + sideLen] - upRight[startY + sideLen + 1][startX + sideLen]
+            # ne = getUpRightSum(startX + sideLen, startY + sideLen, sideLen)
+
+            return sw + nw + se + ne
+        
+
+        m1 = m2 = m3 = float('-inf')
+
+        for row in grid:
+            for x in row:
+                if x > m1:
+                    m1, m2, m3 = x, m1, m2
+                elif x != m1 and x > m2:
+                    m2, m3 = x, m2
+                elif m1 != x != m2 and x > m3:
+                    m3 = x
+            
+        maxSideLen = ((min(width, height) + 1) // 2)
+        # sidelen = a + 1
+        for sideLen in range(1, maxSideLen):
+            for startY in range(sideLen, height - sideLen):
+                for startX in range(0, width - 2* sideLen):
+                    sw = downRight[startY + sideLen][startX + sideLen] - downRight[startY][startX]
+                    # sw = getDownRightSum(startX, startY, sideLen)
+                    nw = upRight[startY - sideLen][startX + 1 + sideLen] - upRight[startY][startX + 1] 
+                    # nw = getUpRightSum(startX + 1, startY - 1, sideLen)
+                    se = downRight[startY + 1][startX + sideLen + 1 + sideLen] - downRight[startY - sideLen + 1][startX + sideLen + 1]
+                    # se = getDownRightSum(startX + sideLen + 1, startY - sideLen + 1, sideLen)
+                    ne = upRight[startY + 1][startX + sideLen + sideLen] - upRight[startY + sideLen + 1][startX + sideLen]
+                    # ne = getUpRightSum(startX + sideLen, startY + sideLen, sideLen)
+
+                    rSum =  sw + nw + se + ne
+                    
+                    # print(f"Rhombussum({startX}, {startY}) = {rSum}")
+                    if rSum > m1:
+                        m1, m2, m3 = rSum, m1, m2
+                    elif rSum != m1 and rSum > m2:
+                        m2, m3 = rSum, m2
+                    elif rSum != m2 and rSum > m3:
+                        m3 = rSum
+
+        if m3 != -inf:
+            return [m1,m2,m3]
+        
+        if m2 != -inf:
+            return [m1, m2]
+
+        return [m1]
