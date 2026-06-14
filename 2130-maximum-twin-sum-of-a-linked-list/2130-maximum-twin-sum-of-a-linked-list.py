cf4e8f5c0a38f24a0f0905dc6c227d45257e45ff
Ashish Kumar <ashiii.2790@gmail.com>
2026-06-14 07:27:25 +0530

    Time: 40 ms (97.82%), Space: 62.9 MB (26.89%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,13 +5,30 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        res, left = 0, head
+        if not head: return None
+        # curr pointer adds the value of the current node to an array
+        # The second moves 2x as fast. 
+        # once the 2x as fast is at the end, the curr pointer is at half.
+        # for the rest of curr, add value to array descending (reverse order)
+        # return largest int in array
+        curr = node = head
+        hold = []
+        while node:
+            node = node.next
+            if node:
+                node = node.next
+                hold.append(curr.val)
+                curr = curr.next
+        #print(f"hold: {hold[::-1]}, curr: {curr.val}")
+        maxVal = -1
+        for i in hold[::-1]:
+            i += curr.val
+            curr = curr.next
+            if i > maxVal: maxVal = i
+            #print(f"i: {i}")
+        #print(f"hold: {hold}")
+        return maxVal
+                
 
-        def dfs(right):
-            nonlocal res, left
-            if right.next: dfs(right.next)
-            res = max(res, left.val + right.val)
-            left = left.next
+
         
-        dfs(head)
-        return res
