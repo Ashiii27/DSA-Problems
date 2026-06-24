db7de610a5cb583e0acf39ff683ee521810b7b45
Ashiii27 <ashiii.2790@gmail.com>
2026-06-24 09:55:14 +0530

    how to create a 2D dynamic array

--- DSA/2D-array.cpp
+++ DSA/2D-array.cpp
@@ -1,20 +1,41 @@
 #include<iostream>
+#include<vector>
+
 using namespace std;
 
 int main() {
-    int m,n;
+    // int m,n;
+    // cout << "Enter the number of rows and columns: ";
+    // cin >> m >> n;
+    // int a[10][10]; // Assuming a maximum size of 10x10
+    // for (int i = 1; i <= m; i++) {
+    //     for (int j = 0; j < n; j++){
+    //         cin >> a[i][j];
+    //     }
+    // }
+    // cout << "--------------------------------------------" << endl;
+    // for (int i = 1; i <= m; i++) {
+    //     for (int j = 0; j < n; j++){
+    //         cout << a[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+//How to make 2D dyanmic vector
+
+    int m, n;
     cout << "Enter the number of rows and columns: ";
     cin >> m >> n;
-    int a[10][10]; // Assuming a maximum size of 10x10
-    for (int i = 1; i <= m; i++) {
-        for (int j = 0; j < n; j++){
-            cin >> a[i][j];
+    vector<vector<int>> x(m, vector<int>(n));
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> x[i][j];
         }
     }
     cout << "--------------------------------------------" << endl;
-    for (int i = 1; i <= m; i++) {
-        for (int j = 0; j < n; j++){
-            cout << a[i][j] << " ";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << x[i][j] << " ";
         }
         cout << endl;
     }
