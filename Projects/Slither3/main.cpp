1fd72d3e2facdee24bccb943ca77718137758270
Ashish <ashiii.2790@gmail.com>
2026-06-27 11:00:03 +0530

    started with curses library

--- Projects/Slither3/main.cpp
+++ Projects/Slither3/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include<curses.h>
 
 using namespace std;
 
 int main() {
-    cout<<"Hello"<<endl;
+    // int width = 20;
+    // int height = 20;
+
+    // for(int i = 0 ; i < width-1 ; i++) {
+    //     cout<<"# ";
+    // }
+    // cout<<endl;
+    // for(int i =1 ; i < height-1 ; i++){
+    //     cout<<"#";
+    //     for(int j = 1 ; j < width-1 ; j++){
+    //         cout<<"  ";
+    //     }
+    //     cout<<"#";
+    //     cout<<endl;
+    // }
+    
+    // for(int i = 0 ; i < width-1 ; i++) {
+    //     cout<<"# ";
+    // }
+    // cout<<endl;
+
+    initscr();
+    move(10,10);
+    addch('#');
+    getch();
+    // cout<<"# ";
+    endwin();
     return 0;
 }
