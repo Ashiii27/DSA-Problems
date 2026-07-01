57551ec8ade3ca3a8fcc2c64098be9701062d66b
Ashiii27 <ashiii.2790@gmail.com>
2026-07-01 07:16:52 +0530

    updated the repo

---
+++ DSA/Strings/basicStrings.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+using namespace std;
+
+int main() {
+    
+    char ch[5]= {'h','e','l','l','o'};
+    //cout << ch << endl;
+    //char ch1[5] = "hello"; // we can't do this as the null char can't get the slot allocated
+    char ch2[6] = "hello"; // this is correct
+    //cout << ch1 << endl;
+    cout << ch2 << endl;
+    return 0;
+}
