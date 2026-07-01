b0f6030f92d8d17f5bc1ef11b3346118fc654167
Ashiii27 <ashiii.2790@gmail.com>
2026-07-01 07:54:41 +0530

    problems on strings

--- DSA/Strings/basicStrings.cpp
+++ DSA/Strings/basicStrings.cpp
@@ -10,11 +10,20 @@ using namespace std;
 
 int main() {
     
-    char ch[5]= {'h','e','l','l','o'};
+    /*char ch[5]= {'h','e','l','l','o'};
     //cout << ch << endl;
     //char ch1[5] = "hello"; // we can't do this as the null char can't get the slot allocated
     char ch2[6] = "hello"; // this is correct
     //cout << ch1 << endl;
     cout << ch2 << endl;
-    return 0;
+    return 0;  */
+
+    //string - set of characters ,  you can change the characters
+    //string literal - a set of characters , it is not editable
+
+    string a = "hello";
+    auto i = "abc";
+
+    reverse(a.begin(),a.end());  //for reversing a string
+    cout << a;
 }
---
+++ DSA/Strings/solutions.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+using namespace std;
+
+int main() {
+
+    int tc;
+    cin >> tc;
+
+    while(tc--) {
+        string s;
+        cin >> s;
+
+        int n = s.length();
+
+        if(n > 10){
+            cout << s[0] << n - 2 << s[n - 1] << endl;
+        } else{
+            cout << s << endl;
+        }
+
+    }
+
+        return 0;
+}
