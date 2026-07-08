8e250131d52ae1f502f03e61565b0327a34751f1
Ashish Kumar <ashiii.2790@gmail.com>
2026-07-08 08:48:42 +0530

    Time: 2 ms (35.94%), Space: 8.1 MB (93.99%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) { return rev(n, 32); }
+    int reverseBits(int n) {
+        int res = 0;
+        uint cur = 31;
 
-private:
-    uint32_t rev(uint32_t v, int len) {
-        if (len == 1) return v & 1u;
-
-        int half = len >> 1;
-        uint32_t mask = (1u << half) - 1u;
-        uint32_t lo = v & mask;
-        uint32_t hi = v >> half;
-
-        return (rev(lo, half) << half) | rev(hi, half);
+        while (n) {
+            if (n & 1) {
+                res += 1 << cur;
+            }
+            --cur;
+            n >>= 1;
+        }
+        return res;
     }
 };
