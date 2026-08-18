7e1ec274eee780c60868e993ce4063e4bc213ee1
Ashiii27 <ashiii.2790@gmail.com>
2026-08-18 22:40:01 +0530

    changed the location of problems solved

--- LEETCODE Problems/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ LEETCODE Problems/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,20 +1,48 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> subNum;
-        subNum[0] = 1;
-        int total = 0, count = 0;
 
-        for (int n : nums) {
-            total += n;
+        unordered_map<int,int> prefixCount;
 
-            if (subNum.find(total - k) != subNum.end()) {
-                count += subNum[total - k];
+        // Base case: A prefix sum of 0 has occurred once (before starting the array)
+        prefixCount[0] = 1;    
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            prefixSum += nums[i];
+
+            int required = prefixSum - k;
+
+            // Check if (prefixSum - k) occurred previously
+            if(prefixCount.find(required) != prefixCount.end()) {
+                count += prefixCount[required];
             }
 
-            subNum[total]++;
+            // Record the current prefix sum count
+            prefixCount[prefixSum]++;
         }
-
         return count;
     }
 };
+
+
+/*    ***********   Brute Force    **********
+
+      int possibleSubArray = 0;
+        
+      for(int i = 0; i < nums.size(); i++) {     
+        int sum = 0;
+        for(int j = i; j < nums.size(); j++) {
+            sum += nums[j];
+            if(sum == k) {
+                possibleSubArray++;
+            }
+        }
+      }
+      return possibleSubArray;
+
+      //Time Complexity = O(n2) , Space Complexity = O(n)
+
+    */
